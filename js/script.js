f1f48690c45e7c3164dc763a944d8ec55ef83144
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-05-26 17:48:50 -0300

    pages and input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const input = document.querySelector("input");
+const inputValue = input.value;
+const searchIcon = document.querySelector(".search i");
+const secondPage = document.querySelector(".secondPage");
+const firstPage = document.querySelector(".firstPage");
 let dataJson = []; 
+
 
 async function fetchData() {
     try {
@@ -15,41 +20,53 @@ async function fetchData() {
     }
 }
 
-async function displayCharacter(){
-    dataArray = await fetchData();
-    dataSlice = dataArray.slice(0,20)
-    dataSlice.forEach(data => {
-        console.log(data)
-        
-        const div = document.querySelector(".content");
+function renderCharacter(characters){
+    const div = document.querySelector(".content");
+    div.innerHTML = "";
+    characters.forEach(data => {
         const newImg = document.createElement("img");
         newImg.src = data.image;
-        console.log(data.image)
-        div.appendChild(newImg); 
-      
-})}
-displayCharacter()
-const inputValue = input.value;
+        div.appendChild(newImg);
+    });
+}
+
+
+
+async function displayFirstPage(){
+    const div = document.querySelector(".content");
+    div.classList.remove("activeImg")
+    dataArray = await fetchData();
+    dataSlice = dataArray.slice(0,16)
+    renderCharacter(dataSlice)
+}
+
+
+
+async function displaySecondPage(){
+    const div = document.querySelector(".content");
+    dataArray = await fetchData();
+    div.classList.add("activeImg")
+    dataSlice = dataArray.slice(16,20)
+    renderCharacter(dataSlice)
+}
+
 
 async function findCharacter(){
     
     const inputValue = input.value.toLowerCase();
     const dataSlice = dataJson.slice(0,20)
     const filteredData = dataSlice.filter(data => data.name.toLowerCase().includes(inputValue));
-    
+    console.log(filteredData)
     const div = document.querySelector(".content");
     div.innerHTML = "";  
 
-    if(filteredData){
-        filteredData.forEach(data => {
-            const newImg= document.createElement("img");
-            newImg.src= data.image;
-            div.appendChild(newImg);
-        });
-    } else{
-        console.log("Personagem não encontrado")
-    }
+    renderCharacter(filteredData)
+   
 }
-const searchIcon = document.querySelector(".search i");
+
 
 input.addEventListener("keyup", () => findCharacter())
+firstPage.addEventListener("click", displayFirstPage)
+secondPage.addEventListener("click", displaySecondPage)
+
+displayFirstPage()
