2fe95e4fca5d9cffc4bc94b9633cc3f0497b95c7
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-05-28 14:18:47 -0300

    working on pushState

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const searchIcon = document.querySelector(".search i");
 const secondPage = document.querySelector(".secondPage");
 const firstPage = document.querySelector(".firstPage");
 let dataJson = []; 
+let allCharacters = [];
+const imgs = [];
 
 
 async function fetchData() {
@@ -27,10 +29,22 @@ function renderCharacter(characters){
         const newImg = document.createElement("img");
         newImg.src = data.image;
         div.appendChild(newImg);
-    });
+        imgs.push(newImg);
+        allCharacters.push(data);
+        newImg.addEventListener("click", (e) => {
+           fetchPage(e.target, [data.name, data.house])
+    })
+   });
 }
 
-
+function fetchPage(target, characteristics){
+  if(target){
+    history.pushState(null,null, 'character.html')
+    const content = document.querySelector(".content");
+    content.innerHTML = characteristics[0]
+  }
+}
+console.log(imgs)
 
 async function displayFirstPage(){
     const div = document.querySelector(".content");
@@ -46,7 +60,7 @@ async function displaySecondPage(){
     const div = document.querySelector(".content");
     dataArray = await fetchData();
     div.classList.add("activeImg")
-    dataSlice = dataArray.slice(16,20)
+    dataSlice = dataArray.slice(16,25)
     renderCharacter(dataSlice)
 }
 
@@ -65,8 +79,10 @@ async function findCharacter(){
 }
 
 
+
+
 input.addEventListener("keyup", () => findCharacter())
 firstPage.addEventListener("click", displayFirstPage)
 secondPage.addEventListener("click", displaySecondPage)
-
 displayFirstPage()
+
